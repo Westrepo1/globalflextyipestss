d9fb562f405489548dcd4cad110fe8fa16c73838
Pius Polocha <117569002+Piuscoding@users.noreply.github.com>
2024-09-04 13:24:32 +0100

    Update userController.js

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -134,9 +134,9 @@ module.exports.aboutPage = (req, res)=>{
 
 
 module.exports.register_post = async (req, res) =>{
-    const {fullname, email,account, country, gender,tel, password, } = req.body;
+    const {fullname, email,account, country, gender,tel,currency, password, } = req.body;
     try {
-        const user = await User.create({fullname, email,account, country, gender,tel, password});
+        const user = await User.create({fullname, email,account, country, gender,tel,currency, password});
         const token = createToken(user._id);
         res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
         res.status(201).json({ user: user._id });
