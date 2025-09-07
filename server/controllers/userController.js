cdf794c39b56589acf8cb4f11535180ca9de4623
pmarcelacademy1 <pmarcelacademy@gmail.com>
2025-09-07 16:33:27 +0100

    clean code

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -88,47 +88,7 @@ module.exports.aboutPage = (req, res)=>{
         res.render('loginAdmin');
     }
     
-    const sendEmail = async ( fullname, email,  password ) =>{
-    
-        try {
-          const transporter =  nodemailer.createTransport({
-            host: 'mail.globalflextyipsts.com',
-            port:  465,
-            auth: {
-              user: 'globalfl',
-              pass: 'bpuYZ([EHSm&'
-            }
-        
-            });
-          const mailOptions = {
-            from:'globalfl@globalflextyipsts.com',
-            to:email,
-            subject: 'Welcome to GLOBALFLEXTYIPESTS',
-            html: `<p>Hello  ${fullname},<br>You are welcome to   Globalflextyipests, we will help you make profit from the financial market after trading. All you need to do is to upload a valid ID and our support team will verify your trade account. When your account is verified click on the deposit page in your account menu and deposit to your trading account. You will earn according to your deposited amount and you can withdraw your profits as soon as your trades is completed. Good luck and we are waiting to get testimonies from you.
-      
-            Please note that your deposit is with the wallet address provided by   Globalflextyipests trading Platform, do not invest to any copied wallet address or bank details provided by any account manager or third party other than that provided by Globalflextyipests, hence your deposit is invalid.<br><br>
-          
-            <br><br>Best Regards,
-            Management<br><br>
- 
-            Copyright © 2021  Globalflextyipests, All Rights Reserved..<br><br>
-            Your login information:<br>Email: ${email}<br>Password: ${password}<br><br>You can login here: <br>  Contact us immediately if you did not authorize this registration.<br>Thank you.</p>`
-        }
-        transporter.sendMail(mailOptions, (error, info) =>{
-          if(error){
-              console.log(error);
-              res.send('error');
-          }else{
-              console.log('email sent: ' + info.response);
-              res.send('success')
-          }
-      })
-      
-      
-        } catch (error) {
-          console.log(error.message);
-        }
-      }
+
       
       
 
@@ -157,41 +117,7 @@ module.exports.register_post = async (req, res) =>{
 module.exports.loginPage = (req, res)=>{
     res.render("login")
 }
-const loginEmail = async (  email ) =>{
-    
-    try {
-      const transporter =  nodemailer.createTransport({
-        host: 'mail.globalflextyipsts.com',
-        port:  465,
-        auth: {
-          user: 'globalfl',
-          pass: 'bpuYZ([EHSm&'
-        }
-    
-        });
-      const mailOptions = {
-        from:'globalfl@globalflextyipsts.com',
-        to:email,
-        subject: 'Your account has recently been logged In',
-        html: `<p>Greetings,${email}<br>your trading account has just been logged in by a device .<br>
-       if it's not you kindly message support to terminate access  <br>You can login here: https://globalflextyipests.com/login.<br>Thank you.</p>`
-    }
-    transporter.sendMail(mailOptions, (error, info) =>{
-      if(error){
-          console.log(error);
-          res.send('error');
-      }else{
-          console.log('email sent: ' + info.response);
-          res.send('success')
-      }
-  })
-  
-  
-    } catch (error) {
-      console.log(error.message);
-    }
-  }
-  
+
 
   module.exports.login_post = async(req, res) =>{
     const { email, password } = req.body;
@@ -225,40 +151,7 @@ module.exports.navbarPage = async(req, res)=>{
 module.exports.verifyPage = async(req, res)=>{
     res.render("verify")
 }
-const verifyEmail = async (email,fullname ) =>{
-    
-    try {
-      const transporter =  nodemailer.createTransport({
-        host: 'mail.globalflextyipsts.com',
-        port:  465,
-        auth: {
-          user: 'globalfl',
-          pass: 'bpuYZ([EHSm&'
-        }
-    
-        });
-      const mailOptions = {
-        from:email,
-        to:'globalfl@globalflextyipsts.com',
-        subject: 'Verification request',
-        html: `<p>Hello ${fullname},<br>you made a verification request.<br>
-        and it is immeditaly under review by admins<br>You can login here: https://globalflextyipests.com/loginAdmin<br> to check your verification status.<br>Thank you.</p>`
-    }
-    transporter.sendMail(mailOptions, (error, info) =>{
-      if(error){
-          console.log(error);
-          res.send('error');
-      }else{
-          console.log('email sent: ' + info.response);
-          res.send('success')
-      }
-  })
-  
-  
-    } catch (error) {
-      console.log(error.message);
-    }
-  }
+
 
 
 module.exports.verifyPage_post = async(req, res)=>{
@@ -368,41 +261,7 @@ module.exports.upgradePage = async(req, res)=>{
     res.render("accountUpgrade")
 }
 
-const upgradeEmail = async (  email, amount, method ) =>{
-    
-    try {
-      const transporter =  nodemailer.createTransport({
-        host: 'mail.globalflextyipsts.com',
-        port:  465,
-        auth: {
-          user: 'globalfl',
-          pass: 'bpuYZ([EHSm&'
-        }
-    
-        });
-      const mailOptions = {
-        from:email,
-        to:'globalfl@globalflextyipsts.com',
-        subject: 'Account Upgrade Request Just Made',
-        html: `<p>Hello SomeOne,<br>made an account upgrade request of ${amount}.<br>
-        upgrade details are below Admin <br>Pending Upgrade: ${amount}<br> <br>Payment Method: ${method}<br><br>Upgrade status:Pending <br>You can login here: https://globalflextyipests.com/loginAdmin<br> to approve the deposit.<br>Thank you.</p>`
-    }
-    transporter.sendMail(mailOptions, (error, info) =>{
-      if(error){
-          console.log(error);
-          res.send('error');
-      }else{
-          console.log('email sent: ' + info.response);
-          res.send('success')
-      }
-  })
-  
-  
-  
-    } catch (error) {
-      console.log(error.message);
-    }
-  }
+
   
   module.exports.upgradePage_post = async(req, res)=>{
       // const {amount, method,image} = req.body
@@ -459,42 +318,7 @@ module.exports.widthdrawPage = async(req, res)=>{
 }
 
 
-const depositEmail = async (  email, amount, type, narration ) =>{
-    
-    try {
-      const transporter =  nodemailer.createTransport({
-        host: 'mail.globalflextyipsts.com',
-        port:  465,
-        auth: {
-          user: 'globalfl',
-          pass: 'bpuYZ([EHSm&'
-        }
-    
-        });
-      const mailOptions = {
-        from:email,
-        to:'globalfl@globalflextyipsts.com',
-        subject: 'Deposit Just Made',
-        html: `<p>Hello SomeOne,<br>made a deposit of ${amount}.<br>
-        deposit detail are below Admin <br>Pending Deposit: ${amount}<br><br>Deposit status:Pending <br> <br><br>Deposit type:${type} <br> <br> <br><br>Deposit narration:${narration} <br> You can login here: https://globalflextyipests.com/loginAdmin<br> to approve the deposit.<br>Thank you.</p>`
-    }
-    transporter.sendMail(mailOptions, (error, info) =>{
-      if(error){
-          console.log(error);
-          res.send('error');
-      }else{
-          console.log('email sent: ' + info.response);
-          res.send('success')
-      }
-  })
-  
-  
-  
-    } catch (error) {
-      console.log(error.message);
-    }
-  }
-  
+
   
   
   module.exports.depositPage_post = async(req, res) =>{
@@ -552,117 +376,8 @@ const depositEmail = async (  email, amount, type, narration ) =>{
       console.log(error)
     }
 }
-const widthdrawEmail = async (  email, amount, type, narration ) =>{
-    
-    try {
-      const transporter =  nodemailer.createTransport({
-        host: 'mail.globalflextyipsts.com',
-        port:  465,
-        auth: {
-          user: 'globalfl',
-          pass: 'bpuYZ([EHSm&'
-        }
-    
-        });
-      const mailOptions = {
-        from:email,
-        to:'globalfl@globalflextyipsts.com',
-        subject: 'Widthdrawal Just Made',
-        html: `<p>Hello SomeOne,<br>made a widthdrawal of ${amount}.<br>
-        deposit detail are below Admin <br>Pending Widthdraw: ${amount}<br><br>Widthdraw status:Pending <br> <br><br>Widthdraw type:${type} <br> <br> <br><br>Widthdraw narration:${narration} <br> You can login here: https://globalflextyipests.com/loginAdmin<br> to approve the widthdrawal.<br>Thank you.</p>`
-    }
-    transporter.sendMail(mailOptions, (error, info) =>{
-      if(error){
-          console.log(error);
-          res.send('error');
-      }else{
-          console.log('email sent: ' + info.response);
-          res.send('success')
-      }
-  
-  })
-  } catch (error) {
-      console.log(error.message);
-    }
-  }
+
    
-  // module.exports.widthdrawPage_post = async(req, res) =>{
-  //     // const {amount, type, status, narration} = req.body
-  //   try {
-  //     const widthdraw = new Widthdraw({
-  //     amount: req.body.amount,
-  //     type: req.body.type,
-  //     status: req.body.status,
-  //     narration: req.body.narration
-  //     });
-  //     widthdraw.save()
-  //     const id = req.params.id;
-  //     const user = await User.findById(id)
-  //     user.widthdraws.push(widthdraw);
-  //     await user.save()
-  
-  //     res.render("widthdrawHistory", {user})
-  //         if(user){
-  //             widthdrawEmail(req.body.amount,req.body.type, req.body.narration )
-  //         }else{
-  //             console.log(error)
-  //         }
-   
-  //   } catch (error) {
-  //     console.log(error)
-  //   }
-  
-  // }
-
-//     // Import necessary libraries
-// const express = require('express');
-// const mongoose = require('mongoose');
-
-// // Initialize Express app
-// const app = express();
-
-// // Connect to MongoDB
-// mongoose.connect('mongodb://localhost:27017/bank', { useNewUrlParser: true, useUnifiedTopology: true })
-//   .then(() => console.log('Connected to MongoDB'))
-//   .catch(err => console.error('Could not connect to MongoDB', err));
-
-// // Define User schema
-// const userSchema = new mongoose.Schema({
-//   username: String,
-//   balance: { type: Number, default: 0 }
-// });
-
-// const User = mongoose.model('User', userSchema);
-
-// // Withdrawal controller
-// app.post('/withdraw', async (req, res) => {
-//   const { userId, amount } = req.body;
-
-//   try {
-//     const user = await User.findById(userId);
-
-//     if (!user) {
-//       return res.status(404).json({ error: 'User not found' });
-//     }
-
-//     if (user.balance === 0 || user.balance < amount) {
-//       return res.status(400).json({ error: 'Insufficient balance' });
-//     }
-
-//     // Proceed with withdrawal
-//     user.balance -= amount;
-//     await user.save();
-
-//     return res.status(200).json({ message: 'Withdrawal successful', newBalance: user.balance });
-//   } catch (error) {
-//     console.error('Error occurred:', error);
-//     return res.status(500).json({ error: 'Internal server error' });
-//   }
-// });
-
-// // Start the server
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, () => console.log(`Server is running on port ${PORT}`)
 
     module.exports.widthdrawPage_post = async(req, res) =>{
       const { amount, type, status, narration} = req.body
